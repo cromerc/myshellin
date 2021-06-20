819b618e0f5665e5545e2458f02e99f067a76a28
Chris Cromer <chris@cromer.cl>
2021-06-19 20:23:49 -0400

    add protection to headers

--- src/include/loop.h
+++ src/include/loop.h
@@ -1,3 +1,6 @@
+#ifndef _MYSHELLIN_LOOP
+#define _MYSHELLIN_LOOP
 void remove_new_line(char *line);
 void print_input_line();
 void loop();
+#endif
--- src/include/user.h
+++ src/include/user.h
@@ -1,1 +1,4 @@
+#ifndef _MYSHELLIN_USER
+#define _MYSHELLIN_USER
 char *get_user();
+#endif
