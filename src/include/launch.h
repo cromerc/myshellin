5506e06840f0cebbfa8ba97b5a74f41b4d5d6479
Chris Cromer <chris@cromer.cl>
2021-06-27 22:46:03 -0400

    add basic launch functionality

--- src/include/launch.h
+++ src/include/launch.h
@@ -13,50 +13,9 @@
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#define _GNU_SOURCE
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "array.h"
-#include "builtins.h"
-#include "console_line.h"
-#include "launch.h"
 
-/**
- * This is the loop that checks for user input and acts on it.
- */
-void loop() {
-    while (1) {
-        print_input_line();
-
-        char *line = get_console_input();
-
-        StringArray args;
-        create_string_array(&args);
-
-        char *saveptr = NULL;
-        char *token = strtok_r(line, " ", &saveptr);
-        while (token) {
-            insert_string_array(&args, token);
-            token = strtok_r(NULL, " ", &saveptr);
-        }
-        if (line != NULL) {
-            free(line);
-            line = NULL;
-        }
-
-        // The user didn't type anything so restart the loop
-        if (args.size == 0) {
-            continue;
-        }
-
-        if (is_builtin(args.array[0])) {
-            run_builtin(&args);
-        }
-        else {
-            launch_program(&args);
-        }
-
-        free_string_array(&args);
-    }
-}
+#ifndef _MYSHELLIN_LAUNCH
+#define _MYSHELLIN_LAUNCH
+void launch_program(StringArray *args);
+#endif
